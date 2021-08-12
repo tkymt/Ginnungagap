8497f860a739021b7c3657aa957cf25ad893adc6
Taku Yamamoto <jts0ymt@gmail.com>
2021-08-12 15:49:16 +0900

    非同期のシーン読み込みを書いた

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Cysharp.Threading.Tasks;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	async void LoadScene(string sceneName)
+	{
+		await SceneManager.LoadSceneAsync(sceneName);
+	}
 }
